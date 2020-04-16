77710d1a9dbd7403102daf899f5d3109027b57b3
Gregory Hawman <g.hawman@gmail.com>
2020-04-16 14:53:39 -0700

    added a lot for baseline files. Barebones server with nothing in it.

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/cookbook.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+};
